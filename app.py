872d5744345776f56b52dcbace4a24e3e976e81f
BozoBZ <dtaraboi@outlook.it>
2024-06-27 20:55:19 +0000

    Created function to retrieve single data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_athletes_from_db
+from database import load_athletes_from_db, load_athlete_from_db
 
 app = Flask(__name__)
        
@@ -13,5 +13,10 @@ def list_athletes():
     athletes = load_athletes_from_db()
     return jsonify(athletes)
 
+@app.route("/athlete/<id_athlete>")
+def show_athlete(id_athlete):
+    athlete = load_athlete_from_db(id_athlete)
+    return jsonify(athlete)
+    
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -13,3 +13,14 @@ def load_athletes_from_db():
                 athletes.append(row._asdict())
         return athletes      
 
+def load_athlete_from_db(id_athlete):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from athletes where idAthlete = :id"), {"id": id_athlete})
+    rows = result.all()
+    if len(rows)==0:
+        return None
+    else:
+        return rows[0]._asdict()
+        
+        
+
