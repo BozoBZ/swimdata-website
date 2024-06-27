4f539dac055d84c360cba3b6949467670ee32b89
BozoBZ <dtaraboi@outlook.it>
2024-06-27 11:46:35 +0000

    Another small

--- database.py
+++ database.py
@@ -4,14 +4,15 @@ import urllib.parse
 user_name = "avnadmin"
 host="swimdata-mysql-swimdata.h.aivencloud.com:25477"
 database = "swimdata_test1"
-password = "AVNS_Nbl8nVYZdZTTu1AuXJj"  # Example password with special characters
+password = "AVNS_Nbl8nVYZdZTTu1AuXJj" 
+
 encoded_password = urllib.parse.quote_plus(password)
 
 connection_string = f"mysql+pymysql://{user_name}:{encoded_password}@{host}/{database}"
 # Or, using engine configuration:
 engine = create_engine(connection_string)
 
-# engine=create_engine("mysql+pymysql://avnadmin:AVNS_Nbl8nVYZdZTTu1AuXJj@swimdata-mysql-swimdata.h.aivencloud.com:25477/swimdata_test1")
+# engine=create_engine("mysql+pymysql://@swimdata-mysql-swimdata.h.aivencloud.com:25477/swimdata_test1")
 
 def load_athletes_from_db():
     with engine.connect() as conn:
