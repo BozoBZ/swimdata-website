ad8703894715f95d4136a1cf2ab7e19cf044f37a
BozoBZ <dtaraboi@outlook.it>
2024-07-02 19:55:13 +0000

    Time Insert to DB via AthletItem-Time_insert_form - time_Submitted

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_athletes_from_db, load_athlete_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_athletes_from_db, load_athlete_from_db, add_time_into_db
 
 app = Flask(__name__)
        
@@ -13,12 +13,21 @@ def list_athletes():
     athletes = load_athletes_from_db()
     return jsonify(athletes)
 
-@app.route("/athlete/<id_athlete>")
-def show_athlete(id_athlete):
-    athlete = load_athlete_from_db(id_athlete)
+@app.route("/athlete/<idAthlete>")
+def show_athlete(idAthlete):
+    athlete = load_athlete_from_db(idAthlete)
     if not athlete:
         return "Not found", 404
     return render_template("athletepage.html", athlete=athlete)
+
+@app.route("/athlete/<idAthlete>/time", methods = ['POST'])
+def insert_time_into_db(idAthlete):
+    data = request.form
+    athlete = load_athlete_from_db(idAthlete)
+    add_time_into_db(idAthlete, data)
     
+    return render_template("time_submitted.html",                                                  application=data,
+                            athlete=athlete)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -13,14 +13,26 @@ def load_athletes_from_db():
                 athletes.append(row._asdict())
         return athletes      
 
-def load_athlete_from_db(id_athlete):
+def load_athlete_from_db(idAthlete):
     with engine.connect() as conn:
-        result = conn.execute(text("select * from athletes where idAthlete = :id"), {"id": id_athlete})
-    rows = result.all()
-    if len(rows)==0:
-        return None
-    else:
-        return rows[0]._asdict()
-        
-        
+        result = conn.execute(text("select * from                     athletes where idAthlete = :id"), {"id": idAthlete})
+        rows = result.all()
+        if len(rows)==0:
+            return None
+        else:
+            return rows[0]._asdict()
 
+def add_time_into_db(idAthlete, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO times (swimevent, idAthlete, idMeet, swimtime, swimtype) VALUES (:swimevent, :idAthlete, :idMeet, :swimtime, :swimtype)")
+        conn.execute(query, 
+                     {
+                        "swimevent" :data['swimevent'],
+                        "idAthlete" :idAthlete,
+                        "idMeet" :data['meet'],
+                        "swimtime" :data['swimtime'],                                          "swimtype" :data['swimtype']
+                     })  
+        conn.commit()
+        
+        
+        
