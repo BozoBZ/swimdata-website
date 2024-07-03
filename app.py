f1f3b30f32719b6bc9c2405a3e5e638818ea703f
BozoBZ <dtaraboi@outlook.it>
2024-07-03 21:01:34 +0000

    Big changes (added times  route, changed home)

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_athletes_from_db, load_athlete_from_db, add_time_into_db
+from database import load_athletes_from_db, load_athlete_from_db, add_time_into_db, load_times_from_db
 
 app = Flask(__name__)
        
 @app.route("/")
 def hello_swimdata():
-    athletes = load_athletes_from_db()
-    return render_template ("home.html", athletes=athletes)
+    return render_template ("home.html")
 
 @app.route("/api/athletes")
-def list_athletes():
+def list_athletes_jsonify():
     athletes = load_athletes_from_db()
     return jsonify(athletes)
 
-@app.route("/athlete/<idAthlete>")
+@app.route("/athletes")
+def list_athletes_tables():
+    athletes = load_athletes_from_db()
+    return render_template ("athletes.html", athletes=athletes)
+
+@app.route("/athletes/<idAthlete>")
 def show_athlete(idAthlete):
     athlete = load_athlete_from_db(idAthlete)
     if not athlete:
         return "Not found", 404
-    return render_template("athletepage.html", athlete=athlete)
+    return render_template("athlete_details.html", athlete=athlete)
 
-@app.route("/athlete/<idAthlete>/time", methods = ['POST'])
+@app.route("/times")
+def list_times_tables():
+    times = load_times_from_db()
+    return render_template ("times.html", times=times)
+
+@app.route("/athletes/<idAthlete>/time", methods = ['POST'])
 def insert_time_into_db(idAthlete):
     data = request.form
     athlete = load_athlete_from_db(idAthlete)
--- database.py
+++ database.py
@@ -11,11 +11,19 @@ def load_athletes_from_db():
         athletes = []
         for row in result.all():
                 athletes.append(row._asdict())
-        return athletes      
+        return athletes
+
+def load_times_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from times"))
+        times = []
+        for row in result.all():
+                times.append(row._asdict())
+        return times      
 
 def load_athlete_from_db(idAthlete):
     with engine.connect() as conn:
-        result = conn.execute(text("select * from                     athletes where idAthlete = :id"), {"id": idAthlete})
+        result = conn.execute(text("select * from                                             athletes where idAthlete = :id"), {"id":                               idAthlete})
         rows = result.all()
         if len(rows)==0:
             return None
@@ -24,15 +32,14 @@ def load_athlete_from_db(idAthlete):
 
 def add_time_into_db(idAthlete, data):
     with engine.connect() as conn:
-        query = text("INSERT INTO times (swimevent, idAthlete, idMeet, swimtime, swimtype) VALUES (:swimevent, :idAthlete, :idMeet, :swimtime, :swimtype)")
+        query = text("INSERT INTO times (swimevent, idAthlete,                            idMeet,swimtime, swimtype) VALUES                                   (:swimevent, :idAthlete, :idMeet, :swimtime,                        :swimtype)")
+        
         conn.execute(query, 
                      {
                         "swimevent" :data['swimevent'],
                         "idAthlete" :idAthlete,
                         "idMeet" :data['meet'],
-                        "swimtime" :data['swimtime'],                                          "swimtype" :data['swimtype']
+                        "swimtime" :data['swimtime'],                                      "swimtype" :data['swimtype']
                      })  
+        
         conn.commit()
-        
-        
-        
